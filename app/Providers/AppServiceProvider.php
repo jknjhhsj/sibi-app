6ee2923477a3b1660bd02d68d8c8f758a9d2999f
jknjhhsj <lely.99th@gmail.com>
2026-07-06 11:58:37 +0700

    force https scheme untuk production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,16 @@
 <?php
 namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
-    public function boot(): void {}
+
+    public function boot(): void
+    {
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
+    }
 }
